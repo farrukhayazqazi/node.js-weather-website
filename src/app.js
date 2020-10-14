26e8dbb17fe2c6ca14df29f5ba343ec10e83f6d1
farrukhayazqazi <54480112+farrukhayazqazi@users.noreply.github.com>
2020-10-13 22:42:12 -0700

    Init commit

---
+++ src/app.js
@@ -0,0 +1,95 @@
+const path = require('path')
+const express =  require('express')
+const hbs = require('hbs')
+const forecast = require('./utils/forecast')
+const geocode = require('./utils/geocode')
+
+
+
+
+const app = express()
+
+// Defining paths for express config.
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('/', (req, res) =>{
+    res.render('index', {
+        title: 'Weather App!',
+        name: 'Farrukh Ayaz'
+    })
+})
+
+app.get('/about', (req, res) =>{
+    res.render('about', {
+        title: 'About Me!',
+        name: 'Farrukh Ayaz'
+    })
+})
+
+app.get('/help', (req, res) =>{
+    res.render('help', {
+        helpText: 'this is some useful text.',
+        title: 'Help',
+        name: 'Farrukh Ayaz'
+    })
+})
+
+app.get('/weather',(req, res) =>{
+
+    if(!req.query.address){
+        return res.send({
+            error: 'Please provide an address!'
+        })
+    } //
+
+    geocode(req.query.address, (error,{latitude, longitude, location} = {}) =>{
+        if(error){
+            return res.send({error})
+        }
+
+        forecast(latitude, longitude, (error, forecastData) =>{
+            if(error){
+                res.send({error})
+            }
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+
+}) //
+
+
+app.get('/help/*', (req, res) =>{
+    res.render('404',{
+        title: '404',
+        errorMessage: 'Help article not found',
+        name: 'Farrukh Ayaz'
+    
+    })
+})
+
+app.get('*', (req, res) =>{
+    res.render('404',{
+        title: '404',
+        errorMessage: 'Page not found :/',
+        name: 'Farrukh Ayaz'        
+    })    
+})
+
+app.listen(3000, () =>{
+    console.log('Server is up and running on port 3000')
+})
+
---
+++ src/utils/forecast.js
@@ -0,0 +1,34 @@
+const request = require('request')
+
+
+const forecast = (latitude, longitude, callback) =>{
+
+    const url = 'http://api.openweathermap.org/data/2.5/find?lat=' + encodeURIComponent(latitude) + '&lon='+ encodeURIComponent(longitude) +'&cnt=50&appid=c530ea95ba130de85feebe34b57c0274&units=metric'
+
+
+
+    request({ url, json: true }, (error, response) =>{
+
+        const { list } = response.body
+
+        if(error){
+            callback('Unable to connect to the weather services', undefined)
+        } else if(list.length === 0){
+            callback('Unable to find the forcast! try another search')
+        } else{
+            callback(undefined, list[0].weather[0].description + '. It is currently '+ list[0].main.temp_max + ' degrees. '+'There is a '+ list[0].clouds.all +'% chance of rain.')
+                // location: list[0].name,
+            //     temprature: response.body.list[0].main.temp_max,
+            //     description: response.body.list[0].weather[0].description,
+            //     rain: response.body.list[0].clouds.all                
+            // })
+        }
+
+
+
+    })
+
+
+}
+
+module.exports = forecast
